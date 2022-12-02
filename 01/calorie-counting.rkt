a9409e20e82a1d647f3d7cc18903c45b220a3b85
Rich Dougherty <rich@rd.nz>
2022-12-02 20:06:14 +1300

    Day 01 - more

--- 01/calorie-counting.rkt
+++ 01/calorie-counting.rkt
@@ -1,7 +1,9 @@
 #lang racket/base
 
 (require data/maybe)
+(require racket/block)
 (require racket/file)
+(require racket/list)
 (require rackunit)
 
 ;;;
@@ -74,18 +76,25 @@
 
 ;;;
 
+(define (sum-list lst)
+  (foldl + 0 lst))
+
+(check-equal? (sum-list '()) 0)
+(check-equal? (sum-list '(1)) 1)
+(check-equal? (sum-list '(1 2)) 3)
+
+;;;
+
 (define (sum-sublists list-of-sublists)
-  (map (λ (sublist)
-         (foldl + 0 sublist))
-       list-of-sublists))
+  (map sum-list list-of-sublists))
 
 (check-equal? (sum-sublists '()) '())
 (check-equal? (sum-sublists '((1))) '(1))
 (check-equal? (sum-sublists '((1 2))) '(3))
 (check-equal? (sum-sublists '((1 2) (3 4) () (5 6 7))) '(3 7 0 18))
 
-(define example-file-summed (sum-sublists example-file-numbers))
-(check-equal? example-file-summed '(6000 4000 11000 24000 10000))
+(define example-file-summed-sublists (sum-sublists example-file-numbers))
+(check-equal? example-file-summed-sublists '(6000 4000 11000 24000 10000))
 
 ;;;
 
@@ -97,21 +106,69 @@
 
 (check-equal? (find-max '(3 7 0 18)) (just 18))
 
-(define example-file-max (find-max example-file-summed))
+(define example-file-max (find-max example-file-summed-sublists))
 (check-equal? example-file-max (just 24000))
 
 ;;;
 
-(define (max-from-file file-name)
+(define (summed-sublists-from-file file-name)
   (let* ([file-lines (file->lines file-name)]
          [split-lines (split-lines-on-empty file-lines)]
          [numbers (sublist-strings->numbers split-lines)]
-         [summed (sum-sublists numbers)]
-         [maybe-max (find-max summed)])
-    (from-just! maybe-max)))
+         [summed-sublists (sum-sublists numbers)])
+    summed-sublists))
 
-(check-equal? (max-from-file "example") 24000)
+
+(check-equal? (summed-sublists-from-file "example") '(6000 4000 11000 24000 10000))
 
 ;;;
 
+(define (max-from-file file-name)
+  (let* ([summed-sublists (summed-sublists-from-file file-name)]
+         [maybe-max (find-max summed-sublists)]
+         [max (from-just! maybe-max)])
+    max))
+
+(check-equal? (max-from-file "example") 24000)
+
+;;; Answer to Part 1
+
 (displayln (max-from-file "input"))
+
+;;; Part 2 ;;;
+
+(define (take-sorted count lst less-than?)
+  (take (sort lst less-than?) count))
+
+(check-equal? (take-sorted 0 '() <) '())
+(check-equal? (take-sorted 0 '(1 2 3) <) '())
+(check-equal? (take-sorted 1 '(1 2 3) <) '(1))
+(check-equal? (take-sorted 2 '(1 2 3) <) '(1 2))
+(check-equal? (take-sorted 3 '(1 2 3) <) '(1 2 3))
+(check-equal? (take-sorted 3 '(1 2 3 4 5) <) '(1 2 3))
+(check-equal? (take-sorted 3 '(1 2 3 4 5) >) '(5 4 3))
+
+
+(define example-file-top-3 (take-sorted 3 example-file-summed-sublists >))
+(check-equal? example-file-top-3  '(24000 11000 10000))
+
+;
+
+(define (sum-top-3 lst)
+  (sum-list (take-sorted 3 lst >)))
+
+(define example-file-top-3-sum (sum-top-3 example-file-summed-sublists))
+(check-equal? example-file-top-3-sum  45000)
+
+;;;
+
+(define (sum-top-3-from-file file-name)
+  (let* ([summed-sublists (summed-sublists-from-file file-name)]
+         [sum (sum-top-3 summed-sublists)])
+    sum))
+
+(check-equal? (sum-top-3-from-file "example") 45000)
+
+;;; Answer to Part 2
+
+(displayln (sum-top-3-from-file "input"))
